2d67104b7d55bb38c917eb893ffd0f9bdac809ae
Murillo-pedroso <murillomonteirop@gmail.com>
2021-04-28 17:37:22 -0300

    prioridade pronta

--- App.java
+++ App.java
@@ -1,16 +1,17 @@
 import java.util.*;
 
 import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class App {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         boolean sair = false;
         boolean error = false;
         boolean preferencia = false;
 
         int respPreferencia = 0;
         int tamanho = 0;
-        
+
         int cont = 0;
         int cont2 = 0;
         Scanner sc = new Scanner(System.in);
@@ -18,81 +19,81 @@ public class App {
 
         System.out.println("Quantos clientes cabem na fila do seu banco?(Limite da fila)");
         tamanho = sc.nextInt();
-        System.out.println("---Você adicionou um limite de: "+tamanho+" clientes a sua fila.");
-        System.out.println("\n Você deseja:\n1 - Adicionar novo cliente na fila\n2 - Chamar o próximo cliente\n3 - Mostrar todos os clientes\n4 - sair");
-        
-        FilaCliente fila = new FilaCliente(tamanho);        
-        do{
-            do{
+        FilaCliente fila = new FilaCliente(tamanho);
+
+        System.out.println("---Você adicionou um limite de: " + tamanho + " clientes a sua fila.");
+        System.out.println(
+                "\n Você deseja:\n1 - Adicionar novo cliente na fila\n2 - Chamar o próximo cliente\n3 - Mostrar todos os clientes\n4 - sair");
+
+        do {
+
+            do {
                 error = false;
-                if (cont2>0){
-                    System.out.println("Você deseja:\n1 - Adicionar novo cliente na fila\n2 - Chamar o próximo cliente\n3 - Mostrar todos os clientes\n4 - sair");
+                if (cont2 > 0) {
+                    System.out.println(
+                            "Você deseja:\n1 - Adicionar novo cliente na fila\n2 - Chamar o próximo cliente\n3 - Mostrar todos os clientes\n4 - sair");
 
                 }
-                
+
                 int resp = sc.nextInt();
-               
-                if (resp == 1){
+
+                if (resp == 1) {
                     System.out.println("---------Opção: ADICIONAR CLIENTE---------");
 
-                    if(fila.isFull() == false){
-                        do{
+                    if (fila.isFull() == false) {
+                        do {
                             System.out.println("\nO cliente é preferencial?\n  0 - SIM\n  1 - NÃO");
-                                
+
                             respPreferencia = sc.nextInt();
-    
+
                             if (respPreferencia == 0) {
-                                preferencia = true;
-                            }else if (respPreferencia == 1){
-                                preferencia = false;
-                            }else{
+                                fila.enqueue(true);
+                                System.out.println("adicionou true");
+
+                            } else if (respPreferencia == 1) {
+                                fila.enqueue(false);
+                                System.out.println("adicionou false");
+                            } else {
                                 System.out.println("-----COMANDO INVÁLIDO-----");
                             }
-                        }while(respPreferencia != 0 && respPreferencia != 1);
-                        
-                        System.out.println(preferencia);
-                        System.out.println(cont);
-    
-                        fila.Enqueue(cont, preferencia); 
-                        cont++;
-
-                    }else{
+                            System.out.println("adicionou false");
+                        } while (respPreferencia != 0 && respPreferencia != 1);
+                        System.out.println("adicionou false");
+                    } else {
 
                         System.out.println("\nA fila está cheia, chame o próximo cliente para abrir mais espaço\n");
                     }
-                    
-                    cont2++; 
-                                
-                }else if(resp == 2){
-                    System.out.println("Opção: PRÓXIMO CLIENTE");
-                    fila.Dequeue();
+
                     cont2++;
 
-                }else if(resp == 3){
+                } else if (resp == 2) {
+                    System.out.println("Opção: PRÓXIMO CLIENTE");
+                    if (fila.isEmpty() == false) {
+                        fila.proximoCliente();
+                    } else {
+                        System.out.println("\nA fila está vazia, espere um novo cliente\n\n");
+                    }
+                    cont2++;
+
+                } else if (resp == 3) {
                     System.out.println("Opção: MOSTRAR CLIENTES");
-                    fila.imprimeFila();   
-                    cont2++; 
-    
-                }else if(resp == 4){
+                    fila.imprimeFila();
+                    cont2++;
+
+                } else if (resp == 4) {
                     System.out.println("Opção: SAIR ");
                     sair = true;
                     cont2++;
-                }else{
+                } else {
                     System.out.println("Opção: INVÁLIDA, INSIRA NOVAMENTE");
                     error = true;
                     cont2++;
 
                 }
 
-            
-            }while(error == true);
+            } while (error == true);
 
-            
-
-        }while(sair == false);
-    
-        
+        } while (sair == false);
 
     }
 }
-
--- FilaCliente.java
+++ FilaCliente.java
@@ -1,109 +1,153 @@
 public class FilaCliente {
 
     private boolean[] preferencia;
-    private int[] dados;
+    private int[] idCliente;
     private int tamanhoVetor;
     private int posFinal;
-  
-  
-    public FilaCliente(int tamanhoMaximo){
-            this.dados = new int[tamanhoMaximo];
-            this.preferencia = new boolean[tamanhoMaximo];
-            this.tamanhoVetor = tamanhoMaximo;
-            this.posFinal = -1;
+    private int id;
+    private int cont = 0;
+
+    public FilaCliente(int tamanhoMaximo) {
+        this.idCliente = new int[tamanhoMaximo];
+        this.preferencia = new boolean[tamanhoMaximo];
+        this.tamanhoVetor = tamanhoMaximo;
+        this.posFinal = -1;
+        this.id = 0;
     }
-       
-    
-    public void destroyFila() {
-        this.dados = null;
-        System.gc();
-    }
-    
+
     public boolean isFull() {
-        
-        if(this.posFinal == (this.tamanhoVetor - 1)) {
-            
+
+        if (this.posFinal == (this.tamanhoVetor - 1)) {
+
             return true;
 
-        }else{
-        
+        } else {
+
             return false;
 
         }
     }
-    
+
+    public boolean peek() {
+        return this.preferencia[0];
+    }
+
     public boolean isEmpty() {
         if (this.posFinal == -1) {
-            
-            return true;
-        }else{
 
-            
+            return true;
+        } else {
+
             return false;
         }
     }
-    
-    public void Enqueue(int valor,boolean preferencia) {
-        if (!isFull()){
 
-            this.dados[this.posFinal + 1] = valor;
+    public void enqueue(boolean preferencia) {
+        if (!isFull()) {
+
+            this.idCliente[this.posFinal + 1] = this.id;
             this.preferencia[this.posFinal + 1] = preferencia;
             this.posFinal++;
+            this.id++;
         }
-        
+
     }
-    
-    public double Dequeue() {
-        if(!isEmpty()) {
-            double elemento = this.dados[0];
+
+    public void enqueue(int id, boolean preferencia) {
+        if (!isFull()) {
+
+            this.idCliente[this.posFinal + 1] = id;
+            this.preferencia[this.posFinal + 1] = preferencia;
+            this.posFinal++;
+
+        }
+
+    }
+
+    public boolean dequeue() {
+        if (!isEmpty()) {
+            double elemento = this.idCliente[0];
             boolean elementoPref = this.preferencia[0];
-    
+
             for (int i = 0; i < this.posFinal; i++) {
-            this.dados[i] = this.dados[i+1]; 
-            this.preferencia[i] = this.preferencia[i+1]; 
+                this.idCliente[i] = this.idCliente[i + 1];
+                this.preferencia[i] = this.preferencia[i + 1];
 
             }
             this.posFinal--;
-            System.out.printf("\nVocê chamou o cliente de senha: "+elemento);
-        
-            if(elementoPref == true){
+
+            return elementoPref;
+        } else {
+            System.out.println("\nA fila está vazia, espere um novo cliente\n\n");
+
+            return false;
+        }
+
+    }
+
+    public void imprimeFila() {
+        if (isEmpty()) {
+            System.out.println("\nvazia\n\n");
+        } else {
+            System.out.println("--------------------------");
+            for (int i = 0; i <= this.posFinal; i++) {
+                System.out.printf("\nCliente" + (i + 1) + " = " + this.idCliente[i] + " | ");
+
+                if (this.preferencia[i] == true) {
+
+                    System.out.printf("PREFERENCIAL");
+
+                } else {
+
+                    System.out.printf("NORMAL");
+
+                }
+            }
+            System.out.printf("\n--------------------------\n");
+        }
+    }
+
+    public void proximoCliente() {
+        boolean pref;
+        int chamou = 0;
+        if (this.cont == 3) {
+            pref = true;
+            cont = 0;
+        } else {
+            pref = false;
+            cont++;
+        }
+        for (int i = 0; i <= this.posFinal; i++) {
+            if (peek() == pref) {
+                dequeue();
+                chamou = 1;
+                System.out.printf("\nVocê chamou o cliente de senha: " + this.idCliente[0]);
+
+                if (pref == true) {
+                    System.out.printf(" ele é um cliente preferencial\n\n");
+                } else {
+                    System.out.printf(" ele é um cliente normal\n\n");
+                }
+                for (int j = i; j <= this.posFinal; j++) {
+                    enqueue(this.idCliente[0], dequeue());
+                }
+                System.out.printf("\n é o looping?");
+                break;
+            } else {
+                enqueue(this.idCliente[0], dequeue());
+            }
+        }
+        if (chamou == 0) {
+            dequeue();
+            System.out.printf("\nVocê chamou o cliente de senha: " + this.idCliente[0]);
+
+            if (this.preferencia[0] == true) {
                 System.out.printf(" ele é um cliente preferencial\n\n");
-            }else{
+            } else {
                 System.out.printf(" ele é um cliente normal\n\n");
             }
-
-            return 0;
-        }else{
-            System.out.println("\nA fila está vazia, espere um novo cliente\n\n");
-            return 0;
         }
 
     }
-    
-    public void imprimeFila() {
-        if(isEmpty()) {
-        System.out.println("\nvazia\n\n");
-        }
-        else{
-        System.out.println("--------------------------");
-        for (int i = 0; i <= this.posFinal; i++ ) {
-        System.out.printf("\nElemento [" + i + "] = " + this.dados[i] + " | ");
 
-        if(this.preferencia[i] == true){
-
-            System.out.printf("PREFERENCIAL");
-
-        }else{
-
-            System.out.printf("NORMAL");
-
-        }
-        }
-        System.out.printf("\n--------------------------\n");
-        }
-    }
-        
-  
-  
-  
 }
